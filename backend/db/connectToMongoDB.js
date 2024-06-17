f635545a56d4bfdc4cb170a10c4cd1919d8b6385
BLUUUUEE <137717684+BLUUUUEE@users.noreply.github.com>
2024-06-17 23:57:49 +0530

    chatt app

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-const connectToMongoDB = async()=>{
-    // console.log(process.env.MONGO_DB_URI);
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("connected to mongo db");
-    } catch (error) {
-        console.log("Error connecting to db",error.message);
-    }
-}
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
+};
 
 export default connectToMongoDB;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
-const generateTokenAndSetCookie = (userId,res)=>{
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"15d",
-    });
+const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "15d",
+	});
 
-// console.log(userId,process.snv.JWT_SECRET,token);
-res.cookie("jwt",token,{
-    maxAge: 15 * 24 * 60 * 1000, // milli sec
-    httpOnly:true, // preverse xss
-    sameSite: "Strict", // CSRF attacks
-    secure: process.env.NODE_ENV !=="development"
-});
+	res.cookie("jwt", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		secure: process.env.NODE_ENV !== "development",
+	});
 };
+
 export default generateTokenAndSetCookie;
